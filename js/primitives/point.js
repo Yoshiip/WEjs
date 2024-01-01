dfcfaaf32e0e54dd3b2621d78d801c10e366fbf8
Yoshiip <myoshipro@gmail.com>
2024-01-01 20:10:00 +0100

    dynamic viewport

--- js/primitives/point.js
+++ js/primitives/point.js
@@ -4,12 +4,25 @@ class Point {
         this.y = y;
     }
 
-    draw(ctx, size = 18, color = "black") {
+    draw(ctx, { size = 18, color = "black", outline = false, fill = false } = {}) {
         const rad = size / 2;
         ctx.beginPath();
         ctx.fillStyle = color;
         ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
         ctx.fill();
+        if(outline) {
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "yellow";
+            ctx.arc(this.x, this.y, rad * 0.6, 0, Math.PI * 2);
+            ctx.stroke();
+        }
+        if(fill) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, rad * 0.4, 0, Math.PI * 2);
+            ctx.fillStyle = "yellow";
+            ctx.fill();
+        }
     }
 
     equals(point) {
